fe9996fd5ebabed07f4dde3dde6bc223d703ab45
littlekids05 <31886719+littlekids05@users.noreply.github.com>
2026-05-17 10:37:22 +0700

    Create app.js

---
+++ src/admin/app.js
@@ -0,0 +1,45 @@
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+  translations: {
+    en: {
+      'Auth.form.welcome.title': 'Welcome to Custom CMS',
+      'Auth.form.welcome.subtitle': 'Sign in to custom cms',
+    },
+  },
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
